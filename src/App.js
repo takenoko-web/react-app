fa51394a7a4357005fed0923d0b6027814785f3b
s.takahashi <shuigaoqiao9@gmail.com>
2022-10-16 09:34:35 +0900

    レンダリング最適化。memo,useCallback,useMemo。

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import ChildArea from './components/ChildArea';
+import { useCallback, useMemo, useState } from 'react';
+import { ChildArea } from './components/ChildArea';
 
 function App() {
   console.log('大人')
@@ -15,6 +15,19 @@ function App() {
   const onClickOpen = () => {
     setOpen(!open)
   }
+  /*　useCallbackを使用しないと、関係ないstateが変更されても、
+      コンポーネントが変更されるため、新しく関数が作成される
+      新しく作成された場合、子はpropsが変更されたとして、再レンダリングされる
+      そこでuseCallbackを使用し、第２引数に監視する要素だけ渡すことで
+      再レンダリングを防ぐことができる
+  */
+  const onClickClose = useCallback(() => {
+    setOpen(false)
+  },[setOpen])
+
+  //変数自体のメモ化　再レンダリングされても変数を改めて定義せず使い回す
+  const temp = useMemo(() => 1+3, [])
+  console.log(temp)
   return (
     <>
       <input onChange={onChangeText} value={text}/>
@@ -22,7 +35,7 @@ function App() {
         <button onClick={onClickIncrement}>インクリメント</button>
         <p>{count}</p>
         <button onClick={onClickOpen}>表示</button>
-        <ChildArea open={open}></ChildArea>
+        <ChildArea open={open} onClickClose={onClickClose}></ChildArea>
       </div>
     </>
   );
--- src/components/ChildArea.jsx
+++ src/components/ChildArea.jsx
@@ -1,21 +1,23 @@
-function ChildArea(props) {
+import { memo } from "react"
+
+//memoで囲むことで、親のstateが更新されても再レンダリングされない
+export const ChildArea = memo((props) => {
     console.log('再レンダリング条件：state更新時,props変更時,親が際レンダリングされた時')
     const style = {
         backgroundColor: 'pink',
         width: '90%',
         height: '200px',
     }
-    const {open} = props
+    const {open, onClickClose} = props
     return (
         <>
             {open ? 
                 <div style={style}>
                     <p>子供</p>
+                    <button onClick={onClickClose}>閉じる</button>
                 </div>
             :
             null}
         </>
     )
-}
-
-export default ChildArea
+})
